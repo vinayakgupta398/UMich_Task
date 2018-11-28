e2d8558d1ae18e9fa48540a6343484bf9503a140
vinayakgupta398 <33853011+vinayakgupta398@users.noreply.github.com>
2018-11-28 17:22:08 -0500

    Update MainClass.java

--- UMich_Task/src/NormalDistribution/MainClass.java
+++ UMich_Task/src/NormalDistribution/MainClass.java
@@ -14,21 +14,28 @@ public class MainClass {
 		Connection con=DriverManager.getConnection("jdbc:h2:"+"./Database/MyDataBase", "root", "password");
 		System.out.println("DataBaseCreated");
 		Statement stmt=con.createStatement();
-		String createTable="CREATE TABLE IF NOT EXISTS TABLE2"
+		String createTable="CREATE TABLE IF NOT EXISTS TABLE1"
 							+"(NDNumber NUMBER)";
 		stmt.executeUpdate(createTable);
 		System.out.println("Table Created");
 		//Form an array list which contains all the ND numbers and store that number in the table
-		ArrayList<Double> list1=new ArrayList<Double>();
 		Random r=new Random();
 		for(int i=0;i<100000;i++)
 		{
-			list1.add(r.nextGaussian()*100+500);
 			String sql="INSERT INTO TABLE1 (NDNumber)"+"VALUES("+r.nextGaussian()*100+500+")";
 			stmt.execute(sql);
 			
 		}
 		System.out.println("Data Entered");
+		System.out.println("Extract data from the table and apply formula");
+		String query1="Select avg(distinct(NDNumber)) as Average from TABLE1";
+		ResultSet x=stmt.executeQuery(query1);
+		if(x.next())
+		{
+			System.out.println(x.getString("Average"));
+		}
+		System.out.println("Task Completed");
+		
 		
 		
 		
